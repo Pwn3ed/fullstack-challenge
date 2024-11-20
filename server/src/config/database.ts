768620ce38cd8636822a32efebdc4008f0e04ff6
Diego Michel Prestes <diegoprestes99@hotmail.com>
2024-11-20 12:53:06 -0300

    :sparkles: feat: added database config file

---
+++ server/src/config/database.ts
@@ -0,0 +1,8 @@
+
+
+class Database {
+    private MYSQL_USER = process.env.MYSQL_USER;
+    private MYSQL_PASSWORD = process.env.MYSQL_PASSWORD;
+    private MYSQL_HOST   = process.env.MYSQL_HOST;
+    private MYSQL_DB = process.env.MYSQL_DB;
+}
