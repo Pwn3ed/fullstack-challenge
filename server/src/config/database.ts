9e65ba28eecb8858fc37d2214ae04490cca1948b
Diego Michel Prestes <diegoprestes99@hotmail.com>
2025-02-16 15:26:09 -0300

    fix db

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import mysql from 'mysql2/promise';
 
 
-const db =  mysql.createConnection({
+const db = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
